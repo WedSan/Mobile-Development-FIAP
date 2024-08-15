afcec33634442b6d0ab58b61205ad9c01f27616a
André <andre.alves.silvazesez@gmail.com>
2024-08-15 00:34:42 -0300

    feat: add exercise one

---
+++ TypeScript-Class/Class-08-14-2024/src/ExerciseOne/data.js
@@ -0,0 +1,8 @@
+"use strict";
+let valueOne = "I'am a string";
+let valueTwo = 86;
+let valueThree = true;
+let randomValue = 3123671736187361;
+console.log(valueOne, valueTwo, valueThree, randomValue);
+randomValue = ["random", "array"];
+console.log(randomValue);
---
+++ TypeScript-Class/Class-08-14-2024/src/ExerciseOne/data.ts
@@ -0,0 +1,10 @@
+let valueOne: string = "I'am a string";
+let valueTwo: number = 86;
+let valueThree: boolean = true;
+let randomValue: any = 3123671736187361;
+
+
+console.log(valueOne, valueTwo, valueThree, randomValue);
+randomValue = ["random", "array"]
+console.log(randomValue)
+
