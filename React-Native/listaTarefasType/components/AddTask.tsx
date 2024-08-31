f1c9f286ad44a64e48671c2d86430e744ee5364a
WedSan <andre.alves.silvazesez@gmail.com>
2024-08-30 22:41:25 -0300

    feat: add component to add task into to-do list

---
+++ React-Native/listaTarefasType/components/AddTask.tsx
@@ -0,0 +1,37 @@
+import { View, Input, Text } from "native-base";
+import { IconButton } from "native-base";
+import { useState } from "react";
+import { Ionicons } from "@expo/vector-icons";
+
+interface AddTaskProps {
+    onAddTask: (task: string) => void;
+}
+
+export const AddTask: React.FC<AddTaskProps> = ({ onAddTask }) => {
+
+    const [taskName, setTaskName] = useState<string>("");
+
+    return (
+        <View style={{ backgroundColor: '#402291', paddingVertical: 20, paddingHorizontal: 20, paddingTop: 50 }}>
+            <Text fontSize="xl" color="white" mb={4}>To-Do List</Text>
+            <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+                <View style={{ flex: 1, marginRight: 10 }}>
+                    <Input
+                        placeholder="Digite uma tarefa"
+                        placeholderTextColor="white"
+                        value={taskName}
+                        onChangeText={(taskText) => setTaskName(taskText)}
+                        fontSize={14}
+                        color="white"
+                    />
+                </View>
+                <IconButton
+                    icon={<Ionicons name="add" size={24} color="#402291" />}
+                    colorScheme="light"
+                    onPress={() => onAddTask(taskName)}
+                    style={{ borderRadius: 50, backgroundColor: 'gold' }}
+                />
+            </View>
+        </View>
+    );
+};
