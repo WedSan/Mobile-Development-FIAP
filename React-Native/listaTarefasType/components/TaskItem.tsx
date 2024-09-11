b50264b866785d101d1d4d3cc00fc3f71e1ff0f8
WedSan <andre.alves.silvazesez@gmail.com>
2024-09-11 01:57:18 -0300

    feat: implement edit button and delete button in task item

--- React-Native/listaTarefasType/components/TaskItem.tsx
+++ React-Native/listaTarefasType/components/TaskItem.tsx
@@ -1,28 +1,74 @@
 import { useState } from "react"
 import { Task } from "../types/TaskType"
-import { Box, Text, Button } from "native-base"
-
+import { Box, Text, Button, View, Input, IconButton } from "native-base"
+import {Ionicons} from "@expo/vector-icons"
+import { useGlobalState } from "../hooks/GlobalState"
 
 export const TaskItem: React.FC<Task> = ({id, title}) =>{
-    const [isEditing, setEditing] = useState<boolean>(false)
 
+    const [isEditing, setEditing] = useState<boolean>(false)    
     const [newTitle, setNewTitle] = useState<string>(title)
 
+    const {editTask, deleteTask} = useGlobalState()
+
+
+    const handleEditPress = ()=>{
+        
+        editTask(id, newTitle)
+        setEditing(false)
+    }
+
     return(
-        <Box
-        flexDirection="row" 
-        justifyContent="space-between" 
-        alignItems="center" 
-        bg="gray.200" 
-        p={4} 
-        my={2} 
-        mx={2} 
-        borderRadius={8} 
-        >
-            <Text flex={3} fontSize={18} overflow={'hidden'}> {title}  </Text>
-            <Button size={'md'} variant={'solid'}>
-                edit
-            </Button>
-        </Box>
+        <View>
+            {
+                isEditing ? (
+                    <Box  
+                    flexDirection="row" 
+                    justifyContent="space-between" 
+                    alignItems="center" 
+                    bg="#F2CC00" 
+                    p={4} 
+                    my={2} 
+                    mx={2} 
+                    borderRadius={8}>  
+                        <Input
+                        borderColor={'purple'}
+                        placeholder="Novo nome tarefa"
+                        placeholderTextColor="black"
+                        style={{color: 'black', borderColor: 'purple'}}
+                        onChangeText={(text)=> setNewTitle(text)}
+                        fontSize={14}
+                        width={'70%'}
+                        color="white"
+                        >
+                        </Input>
+                        <IconButton icon={<Ionicons name="checkbox" size={32} color={"purple"}/>} onPress={()=>handleEditPress()}
+                        ></IconButton> 
+                        
+                    </Box>
+                ) : (<Box
+                    flexDirection="row"     
+                    justifyContent="space-between" 
+                    alignItems="center" 
+                    bg="gray.200" 
+                    p={4} 
+                    my={2} 
+                    mx={2} 
+                    borderRadius={8} 
+                    >
+                        <Text flex={3} fontSize={18} overflow={'hidden'}> {title}  </Text>
+                        
+                        <Button size={'md'} variant={'solid'} onPress={()=> setEditing(true)} mr={'25px'}>
+                            edit
+                        </Button>
+                        <Button size={'md'} backgroundColor={'red.600'} variant={'solid'} onPress={()=> deleteTask(id)}>
+                            delete
+                        </Button>
+                        
+
+                    </Box>)
+            }
+            
+        </View>
     )
 }
