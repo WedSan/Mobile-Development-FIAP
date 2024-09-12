a5bdbee6a1cb26ed7afea6ea9c02f4a9facb24a0
WedSan <andre.alves.silvazesez@gmail.com>
2024-09-11 23:07:05 -0300

    feat: add task into db using sqlite3 through post request

--- Express/lista-tarefas-back-end/server.js
+++ Express/lista-tarefas-back-end/server.js
@@ -1,9 +1,28 @@
 const express = require('express');
-
+const sqlite3 = require('sqlite3').verbose()
 const app = express();
+const db = new sqlite3.Database('task.db')
+
+
+db.serialize(()=>{
+    db.run("CREATE TABLE IF NOT EXISTS task (id INTEGER PRIMARY KEY, name TEXT)");
+});
+
+app.use(express.json());
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, ()=>{
-    console.log("application is running")
-})
+    console.log("application is running");
+});
+
+app.post("/task", (req, res)=>{
+    const {task} = req.body;
+    db.run("INSERT INTO TASK (name) VALUES (?)", [task], (err)=>{
+        if(err){
+            res.status(500).json({error: err.message});
+        }
+        res.status(201).json({id: this.lastID, task});
+    })
+});
+
