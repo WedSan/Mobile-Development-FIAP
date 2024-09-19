ae9455c2da07f50a82905af487bb79c5857a23fc
WedSan <andre.alves.silvazesez@gmail.com>
2024-09-19 01:14:49 -0300

    feat: create table user

--- Express/lista-tarefas-back-end/db/db.js
+++ Express/lista-tarefas-back-end/db/db.js
@@ -4,6 +4,7 @@ const db = new sqlite3.Database('task.db')
 const createTables = ()=> {
     db.serialize(()=>{
         db.run("CREATE TABLE IF NOT EXISTS task (id INTEGER PRIMARY KEY, name TEXT)");
+        db.run("CREATE TABLE IF NOT EXISTS user (id INTEGER PRIMARY KEY, username TEXT UNIQUE, password TEXT, role TEXT)")
     });
 }
 
