91f41aa13eed274570c4be667797e58a028b9524
WedSan <andre.alves.silvazesez@gmail.com>
2024-09-19 02:00:41 -0300

    refactor: add /register in the path to access register user method

--- Express/lista-tarefas-back-end/routes/userRoutes.js
+++ Express/lista-tarefas-back-end/routes/userRoutes.js
@@ -2,6 +2,6 @@ const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController')
 
-router.post("/", userController.registerUser);
+router.post("/register", userController.registerUser);
 
 module.exports = router;
