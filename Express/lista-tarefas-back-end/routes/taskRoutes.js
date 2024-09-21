c8afb91f492f1cda8eaed195ecfec863db7577ea
WedSan <andre.alves.silvazesez@gmail.com>
2024-09-21 02:14:51 -0300

    feat: use middleware to only allow users with admin role to update and delete tasks

--- Express/lista-tarefas-back-end/routes/taskRoutes.js
+++ Express/lista-tarefas-back-end/routes/taskRoutes.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const taskController = require('../controllers/taskController');
 const router = express.Router();
+const onlyAdminMiddleware = require('../middlewares/adminOnlyMiddleware');
 
 router.get("/", taskController.getAllTasks);
 router.get("/:id", taskController.getTaskById);
 router.post("/", taskController.createTask);
-router.put("/:id", taskController.updateTask);
-router.delete("/:id", taskController.deleteTask);
+router.put("/:id", onlyAdminMiddleware, taskController.updateTask);
+router.delete("/:id", onlyAdminMiddleware, taskController.deleteTask);
 
 module.exports = router;
