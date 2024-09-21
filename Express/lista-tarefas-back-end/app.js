6190432a9fe1146c8cac774651b76ad9712301da
WedSan <andre.alves.silvazesez@gmail.com>
2024-09-20 22:33:49 -0300

    feat: add auth middleware in task route to check if user has a valid token

--- Express/lista-tarefas-back-end/app.js
+++ Express/lista-tarefas-back-end/app.js
@@ -2,13 +2,14 @@ const express = require('express');
 const app = express();
 const taskRouter = require('./routes/taskRoutes');
 const userRouter = require("./routes/userRoutes")
-const {createTables} = require('./db/db')
+const {createTables} = require('./db/db');
+const authMiddleware = require('./authMiddleware/authMiddleware');
 
 app.use(express.json());
 
 createTables();
 
-app.use("/api/task", taskRouter);
+app.use("/api/task", authMiddleware, taskRouter);
 app.use("/api/user", userRouter);
 
 module.exports = app
