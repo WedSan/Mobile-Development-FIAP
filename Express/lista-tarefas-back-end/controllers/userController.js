72f639786066070c1c8c7477ee29bb6a4d6aa648
WedSan <andre.alves.silvazesez@gmail.com>
2024-09-21 02:12:21 -0300

    fix: add return in condition to ensure correct execution of register user function

--- Express/lista-tarefas-back-end/controllers/userController.js
+++ Express/lista-tarefas-back-end/controllers/userController.js
@@ -10,7 +10,7 @@ const registerUser = (req, res)=>{
         }
         db.run("INSERT INTO user (username, password, role) VALUES (? , ? , ? )", [username, hashedPassword, role], function(err){
             if(err){
-                res.status(500).json({error : "Error trying to register the user"});
+                return res.status(500).json({error : "Error trying to register the user"});
             }
             res.status(200).json({message: "User created successfully"})
         });
