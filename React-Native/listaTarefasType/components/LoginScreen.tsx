25281657d8e8752c2b60592d4e4054f361f70d1a
WedSan <andre.alves.silvazesez@gmail.com>
2024-10-10 17:44:04 -0300

    feat: add login screen

---
+++ React-Native/listaTarefasType/components/LoginScreen.tsx
@@ -0,0 +1,52 @@
+import React from "react";
+import {Box, Button, IconButton, Input} from "native-base";
+import {AntDesign} from "@expo/vector-icons";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import {useNavigation} from "@react-navigation/native";
+import {StackNavigationProp} from "@react-navigation/stack";
+
+type RootStackParamList = {
+    LoginScreen: undefined;
+    TaskScreen: undefined;
+};
+
+type NavigationProp = StackNavigationProp<RootStackParamList, 'LoginScreen'>;
+
+export const LoginScreen: React.FC = ()=>{
+
+    const [username, setUsername] = React.useState("");
+    const [password, setPassword] = React.useState("");
+    const navigation = useNavigation<NavigationProp>();
+
+
+    const sendLogIn = async () =>{
+        const response: Response = await fetch("http://localhost:5000/api/user/login", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({username: username, password: password})
+        })
+
+        if(!response.ok){
+            throw new Error("Failed to login");
+        }
+
+        const data: {token: string}  = await response.json()
+        const token: string = data.token;
+
+        await AsyncStorage.setItem("token", token);
+        navigation.navigate("TaskScreen");
+    }
+
+    return(
+        <Box>
+            <Input value={username} onChangeText={setUsername} placeholder="Username" />
+            <Input value={password} onChangeText={setPassword} placeholder="Password"></Input>
+            <Box>
+                <Button onPress={sendLogIn} > Send </Button>
+            </Box>
+
+        </Box>
+    )
+}
