03a141e5662bfad043c3ff54aa278e41e535d120
WedSan <andre.alves.silvazesez@gmail.com>
2024-10-10 17:45:45 -0300

    feat: add task screen

---
+++ React-Native/listaTarefasType/components/TaskScreen.tsx
@@ -0,0 +1,21 @@
+import {GlobalStateProvider} from "../hooks/GlobalState";
+import {Box, Center, NativeBaseProvider, Text} from "native-base";
+import {AddTask} from "./AddTask";
+import {ListTasks} from "./ListTasks";
+import React from "react";
+
+export const TaskScreen: React.FC = ()=>{
+    return(
+        <GlobalStateProvider>
+            <Center background="white" >
+                <Box background="white">
+                    <Text fontSize="2xl" color="primary.500" fontWeight="bold" fontFamily="Roboto">
+                        To-Do List App
+                    </Text>
+                </Box>
+            </Center>
+            <AddTask></AddTask>
+            <ListTasks></ListTasks>
+        </GlobalStateProvider>
+    )
+}
