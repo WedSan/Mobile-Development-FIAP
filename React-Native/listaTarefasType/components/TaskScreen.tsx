40b37dbe34e3d537a1956762755528383f72d19b
WedSan <andre.alves.silvazesez@gmail.com>
2024-10-14 15:07:45 -0300

    refactor: move AddTask component into ListTask component

--- React-Native/listaTarefasType/components/TaskScreen.tsx
+++ React-Native/listaTarefasType/components/TaskScreen.tsx
@@ -14,7 +14,6 @@ export const TaskScreen: React.FC = ()=>{
                     </Text>
                 </Box>
             </Center>
-            <AddTask></AddTask>
             <ListTasks></ListTasks>
         </GlobalStateProvider>
     )
